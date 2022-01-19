543af97c3543642dda3d0dd07fda37f1b70ec784
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-01-19 13:33:12 +0100

    Add a main script file

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ECT2 - Bonus Project",
+    page_icon="🌤️",
+    menu_items={"Get Help": None, "Report a bug": None, "About": None,},
+)
+
+st.title("Bonus Project")
